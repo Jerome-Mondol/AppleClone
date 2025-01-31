d3a210b5ba5dadb79ab5a20e89a1cf2dfd338808
Jerome Mondol <jeromemondol11@gmail.com>
2025-02-01 00:15:33 +0600

    Navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import { appleImg, bagImg, searchImg } from '../utils'
+import { navLists } from '../constants'
 
 const Navbar = () => {
   return (
-    <header>
-        <nav>
+    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center ">
+        <nav className="Navbar flex justify-center items-center w-full screen-max-width">
             <img src={appleImg} alt="Apple" width={14} height={18}/>
 
-            <div>
-                {["Phones", "Macbooks", "Tablets"].map((nav, i) => {
-                    return <div key={i}>
+            <div className="flex flex-1 justify-center max-sm:hidden">
+                {navLists.map(nav => (
+                    <div key={nav} className="px-5 text-sm cursor-pointer opacity-75 hover:opacity-100 transition-all">
                         {nav}
                     </div>
-                })}
+                ))}
             </div>
 
-            <div>
+            <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
                 <img src={searchImg} alt="Search" width={18} height={18}/>
                 <img src={bagImg} alt="bag" width={18} height={18}/>
             </div>
